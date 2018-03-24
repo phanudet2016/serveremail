e51dce458a5f881cf8f2d032dea0a256cc307fb8
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2018-03-24 18:47:38 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+var nodemailer = require('nodemailer')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+var new_post
+var email
+var text
+
+app.post('/add_post', (req, res) => {
+	var title = req.body.title;
+  var description = req.body.description;
+  email = req.body.title
+  text = description = req.body.description;
+  new_post = [{
+      title: title,
+      description: description
+  }]
+  // sendEmail();
+})
+
+function sendEmail() {
+    // sendEmail
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      secure: false,
+      port: 25,
+      auth: {
+        user: '5706021622141@fitm.kmutnb.ac.th',
+        pass: '08486787bg'
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+    
+    let HelperOptions = {
+      from: '"Beer" <5706021622141@fitm.kmutnb.ac.th>',
+      to: email,
+      subject: '555',
+      text: text
+    };
+    
+    transporter.sendMail(HelperOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log("The message was sent!");
+        console.log(info);
+    })
+}
+// endSendEmail
+app.get('/posts', (req, res) => {
+  res.send(new_post)
+})
+
+app.listen(process.env.PORT || 8081)
